1221d7fe57f4a988ea004251fd539fbb929f7071
ilias nadmi <ilias.nadmi1@gmail.com>
2026-05-24 02:09:09 +0100

    adding the global handler

---
+++ services/product/src/main/java/com/lyevsky/microservices/product/handler/ErrorResponse.java
@@ -0,0 +1,7 @@
+package com.lyevsky.microservices.product.handler;
+
+import java.util.Map;
+
+public record ErrorResponse(
+        Map<String, String> errors
+) {}
---
+++ services/product/src/main/java/com/lyevsky/microservices/product/handler/GlobalExceptionHandler.java
@@ -0,0 +1,32 @@
+package com.lyevsky.microservices.product.handler;
+
+import com.lyevsky.microservices.product.exceptions.ProductPurshaseException;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
+
+import static org.springframework.http.HttpStatus.BAD_REQUEST;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(ProductPurshaseException.class)
+    public ResponseEntity<String> handleCustomerNotFoundException(ProductPurshaseException exp) {
+        return ResponseEntity.status(BAD_REQUEST).body(exp.getMessage());
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorResponse> handleMethodArgumentNotValidException(MethodArgumentNotValidException exp) {
+        var errors = new HashMap<String, String>();
+        exp.getBindingResult().getAllErrors().forEach(error -> {
+            var fieldName = ((FieldError) error).getField();
+            var errorMessage = error.getDefaultMessage();
+            errors.put(fieldName, errorMessage);
+        });
+        return ResponseEntity.status(BAD_REQUEST).body(new ErrorResponse(errors));
+    }
+}
