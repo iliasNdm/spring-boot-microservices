17a81d67b5c6730c2d54f8ddd6b52dc2fec007e2
ilias nadmi <ilias.nadmi1@gmail.com>
2026-06-04 15:51:03 +0100

    finalizing the orderController

--- services/order/src/main/java/com/lyevsky/microservices/order/order/service/OrderService.java
+++ services/order/src/main/java/com/lyevsky/microservices/order/order/service/OrderService.java
@@ -2,6 +2,8 @@ package com.lyevsky.microservices.order.order.service;
 
 
 import com.lyevsky.microservices.order.customer.CustomerClient;
+import com.lyevsky.microservices.order.kafka.OrderConfirmation;
+import com.lyevsky.microservices.order.kafka.OrderProducer;
 import com.lyevsky.microservices.order.order.dao.OrderRepository;
 import com.lyevsky.microservices.order.order.dto.OrderRequest;
 import com.lyevsky.microservices.order.order.dto.OrderResponse;
@@ -10,11 +12,13 @@ import com.lyevsky.microservices.order.order.mappers.OrderMapper;
 import com.lyevsky.microservices.order.orderline.OrderLineMapper;
 import com.lyevsky.microservices.order.orderline.OrderLineRepository;
 import com.lyevsky.microservices.order.product.ProductClient;
+import jakarta.persistence.EntityNotFoundException;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -25,11 +29,12 @@ public class OrderService {
     private final OrderMapper orderMapper;
     private final OrderLineRepository orderLineRepository;
     private final OrderLineMapper orderLineMapper;
+    private final OrderProducer orderProducer;
 
     public Long createOrder(@Valid OrderRequest request) {
 
 //        check the customer existance  => via open feign
-        customerClient.findCustomerById(request.customerId())
+        var customer = customerClient.findCustomerById(request.customerId())
                 .orElseThrow(() -> new BusinessException("Customer with id " + request.customerId() + " not found"));
 
 //        purshase the products => check the products existance and availability using RestTemplate
@@ -46,13 +51,27 @@ public class OrderService {
 //        todo start the payment process
 
 //        send the order confirmation to our kafka broker
+        orderProducer.sendOrderConfirnmation(new OrderConfirmation(
+                request.reference(),
+                request.amount(),
+                request.paymentMethod(),
+                customer,
+                purchasedProducts
+        ));
+
         return order.getId();
     }
 
-    public OrderResponse findById(Integer orderId) {
-        return null;
+    public OrderResponse findById(Long orderId) {
+        return
+                orderRepository.findById(orderId)
+                .map(orderMapper::fromOrder)
+                        .orElseThrow(() -> new EntityNotFoundException("Order with id " + orderId + " not found"));
     }
     public List<OrderResponse> findAll() {
-        return null;
+        return orderRepository.findAll()
+                .stream()
+                .map(orderMapper::fromOrder)
+                .collect(Collectors.toList());
     }
 }
